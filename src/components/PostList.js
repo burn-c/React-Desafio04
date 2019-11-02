d3493907ab58e3120ceee7af31b72dc815ebc321
burn-c <chopcsom@gmail.com>
2019-11-02 17:50:41 -0300

    Update: Avatar

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,9 @@ import React, { Component} from 'react';
 import PostItem from './PostItem';
 
 import avatarVinci from '../assets/avatarVinci.png';
+import avatarThomas from '../assets/avatarThomas.png';
+import avatarEike from '../assets/avatarEike.png';
+import avatarCollor from '../assets/avatarCollor.png';
 
 class PostList extends Component {
     state = {
@@ -11,7 +14,7 @@ class PostList extends Component {
                 id:1,
                 author: {
                     name: 'Leonardo da Vinci',
-                    avatar: 'https://i.pravatar.cc/150?img=1'
+                    avatar: avatarVinci
                 },
                 date: "01 Mai 2019",
                 content: "Alguém quer comprar patentes?",
@@ -20,7 +23,7 @@ class PostList extends Component {
                         id:1,
                         author: {
                             name: "Thomas",
-                            avatar: 'https://i.pravatar.cc/150?img=1'
+                            avatar: avatarThomas
                         },
                         content: "Tem WhatsApp?"
                     }
@@ -30,7 +33,7 @@ class PostList extends Component {
                 id:2,
                 author: {
                     name: 'Collor',
-                    avatar: 'https://i.pravatar.cc/150?img=1'
+                    avatar: avatarCollor
                 },
                 date: "02 Mai 2019",
                 content: "Alguém quer comprar uma Ferrari?",
@@ -39,9 +42,17 @@ class PostList extends Component {
                         id:1,
                         author: {
                             name: "Eike",
-                            avatar: 'https://i.pravatar.cc/150?img=1'
+                            avatar: avatarEike
                         },
                         content: "Qual modelo? Qual cor?"
+                    },
+                    {
+                        id:2,
+                        author: {
+                            name: "Eike",
+                            avatar: avatarEike
+                        },
+                        content: "Qual forma de pagamento?"
                     }
                 ]
             },
